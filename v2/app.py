8900a8cb1ae0256dd58540eab2cf4e3906652370
Martijn Grendelman <m@rtijn.net>
2012-10-01 23:43:47 +0200

    Add some UI event handlers and fill liststore1

--- v2/app.py
+++ v2/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
+import os
+import pyexiv2
 from gi.repository import Gtk
+from pprint import pprint
 
 class App(object):
 
@@ -17,17 +20,53 @@ class App(object):
 
     def main(self):
         self.setup_gui()
-        #self.ui.treeview0.set_model(self.imagestore)
-        self.window.connect("delete-event", self.quit)
-        #self.ui.menuitem_quit.connect("activate", self.quit)
+        self.setup_gui_events()
+        self.populate_store1(self.imagedir)
         Gtk.main()
 
     def setup_gui(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file("taggert.glade")
         self.window = self.builder.get_object("window1")
+        self.window.set_position(Gtk.WindowPosition.CENTER)
         self.window.show_all()
 
-    def quit(self, _widget, c):
+    def setup_gui_events(self):
+        # X
+        self.window.connect("delete-event", self.quit)
+        # File -> Quit
+        quit = self.builder.get_object("imagemenuitem5")
+        quit.connect("activate", self.quit, False)
+
+    def quit(self, _window, _event):
         print "bla"
         Gtk.main_quit()
+
+    def populate_store1(self, imagedir):
+        store = self.builder.get_object("liststore1")
+        store.clear()
+        if imagedir:
+            for fl in os.listdir(imagedir):
+                if not fl[0] == '.':
+                    fname = os.path.join(imagedir, fl)
+                    if not os.path.isdir(fname):
+                        if os.path.splitext(fname)[1].lower() == ".jpg":
+                            metadata = pyexiv2.ImageMetadata(fname)
+                            metadata.read()
+                            try:
+                                dt = metadata['Exif.Image.DateTime'].raw_value
+                            except KeyError:
+                                dt = ''
+                            try:
+                                rot =  metadata['Exif.Image.Orientation'].raw_value
+                            except KeyError:
+                                rot = '1'
+                            try:
+                                imglat = metadata['Exif.GPSInfo.GPSLatitude'].human_value
+                            except KeyError:
+                                imglat = ''
+                            try:
+                                imglon = metadata['Exif.GPSInfo.GPSLongitude'].human_value
+                            except KeyError:
+                                imglon = ''
+                            store.append([fl, dt, rot, imglat, imglon])
