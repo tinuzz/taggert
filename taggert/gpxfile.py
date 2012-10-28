539f36a51d2f1430a9fe33e76ba3e17793914b71
Martijn Grendelman <m@rtijn.net>
2012-10-28 23:46:46 +0100

    Add license information

--- taggert/gpxfile.py
+++ taggert/gpxfile.py
@@ -2,10 +2,23 @@
 
 # gpximport.py - Used to import GPX XML files into applications
 # This file was taken from GPX Viewer and modified for Taggert
+# Used and relicensed with explicit permission from copyright holder
 # GPX Viewer homepage: http://andrewgee.org/blog/projects/gpxviewer/
 #
 # Original code copyright (C) 2009 Andrew Gee
 # Modifications copyright (C) 2012 Martijn Grendelman
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 from pprint import pprint
 import xml.dom.minidom as minidom
